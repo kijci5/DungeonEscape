546800ba4c8d03a2aa5cf781729ef127c3b107b0
DESKTOP-6JOJD7C\Kuba <brodaczz1@gmail.com>
2018-09-04 21:36:46 +0200

    Added player to the scene and player movement to player script

---
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed=2f;
+
+    private Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float translation = Input.GetAxis("Horizontal")*speed;
+
+        rigidBody.velocity = new Vector2(translation,rigidBody.velocity.y);
+	}
+}
