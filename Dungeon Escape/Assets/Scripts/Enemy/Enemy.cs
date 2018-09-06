1854d74c68744466ecf1dfc518e00e13fedaf0f1
DESKTOP-6JOJD7C\Kuba <brodaczz1@gmail.com>
2018-09-06 13:47:38 +0200

    Moss giant walking between waypoints

---
+++ Dungeon Escape/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    protected int health;
+    [SerializeField]
+    protected int speed;
+    [SerializeField]
+    protected int gems;
+    [SerializeField]
+    protected Transform pointA, pointB;
+
+    private Vector3 currentTarget;
+
+    protected virtual void Attack()
+    {
+
+    }
+
+    protected virtual void Move()
+    {
+        if (transform.position.x <= pointA.position.x)
+        {
+            currentTarget = pointB.position;
+        }
+        else if (transform.position.x >= pointB.position.x)
+        {
+            currentTarget = pointA.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+
+    public abstract void Update();
+}
---
+++ Dungeon Escape/Assets/Scripts/Enemy/MossGiant.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossGiant : Enemy {
+
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public override void Update ()
+	{
+	    base.Move();
+	}
+}
