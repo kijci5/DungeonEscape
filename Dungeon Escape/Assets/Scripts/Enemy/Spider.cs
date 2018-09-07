4079feba1a8e89aca33b9cf23be496a9679a1adf
DESKTOP-6JOJD7C\Kuba <brodaczz1@gmail.com>
2018-09-07 22:39:59 +0200

    Spider animations and ai

--- Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class Spider : Enemy {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	public override void Update () {
-		
-	}
+	    if (IsIdleState())
+	    {
+	        return;
+	    }
+	    base.Move();
+    }
 }
