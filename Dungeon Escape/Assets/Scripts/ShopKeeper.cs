be537ba93bde998eded2cbf5ad6cf73bb7ee1c11
DESKTOP-6JOJD7C\Kuba <brodaczz1@gmail.com>
2018-09-09 17:07:57 +0200

    Shop Keeper,Shop Panel

---
+++ Dungeon Escape/Assets/Scripts/ShopKeeper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class ShopKeeper : MonoBehaviour
+{
+
+    public GameObject shopPanel;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                UIManager.Instance.OpenShop(player.DiamondsCollected);
+            }
+            shopPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            shopPanel.SetActive(false);
+        }
+    }
+}
